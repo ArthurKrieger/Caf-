9eb3de8a7011390d8e655e003791b1faf633d8fe
Arthur <arthurlkrieger@hotmail.com>
2021-02-01 17:36:25 -0300

    Menu Funcionando

---
+++ menuanimation.js
@@ -0,0 +1,19 @@
+function show_drink() {
+    document.getElementById("eat").style.display = "none";
+    document.getElementById("drinks").style.display = "block";
+    document.getElementById("botao_drinks").className = "tab active"
+    document.getElementById("botao_eat").className = "tab "
+
+    console.log("foi drink");
+
+}
+
+function show_eat() {
+    document.getElementById("eat").style.display = "block";
+    document.getElementById("drinks").style.display = "none";
+    document.getElementById("botao_drinks").className = "tab "
+    document.getElementById("botao_eat").className = "tab active"
+    console.log("foi eat");
+
+
+}
